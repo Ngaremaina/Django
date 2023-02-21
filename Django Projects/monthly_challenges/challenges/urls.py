135f005043b3d836f2a68ea082fe5c458c81f1c2
Ngare <45742522+Ngaremaina@users.noreply.github.com>
2023-02-21 15:23:53 +0300

    Django

---
+++ Django Projects/monthly_challenges/challenges/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from challenges import views 
+
+urlpatterns = [
+    # path('january', views.january),
+    # path('february', views.february),
+    # path('march', views.march),
+    # path('april', views.april),
+    path('<int:month>', views.monthly_challenge_by_number),
+    path("<str:month>", views.monthly_challenge, name="month-challenge"),
+]
